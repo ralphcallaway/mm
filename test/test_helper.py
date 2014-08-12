090992b4e7b2d1a3a110cd2c64f1a7deb1b0b9f2
Joe Ferraro <ferraro.joseph@gmail.com>
2014-08-12 11:58:13 -0400

    add circleci config

--- test/test_helper.py
+++ test/test_helper.py
@@ -29,7 +29,7 @@ class MavensMateTest(unittest.TestCase):
         self.saved_stdout = sys.stdout
         sys.stdout = self.output
         self.settings = util.get_plugin_client_settings()
-        is_ci = os.environ.get('TRAVIS') == 'true' or os.environ.get('TRAVIS') == True
+        is_ci = os.environ.get('CI') == 'true' or os.environ.get('CI') == True
         if is_ci:
             self.settings['user']['mm_workspace'] = os.path.join(os.path.dirname(__file__), 'test_workspace')
         PluginConnection.get_plugin_client_settings = mock.Mock(return_value=self.settings)
