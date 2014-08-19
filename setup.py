7e313895436579d198b071d5c7571e29b6e785c6
Joe Ferraro <ferraro.joseph@gmail.com>
2014-08-19 12:13:48 -0400

    push version [skip ci]

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_require = ['pytest', 'virtualenv>=1.10', 'scripttest>=1.3', 'mock']
 
 setup(
     name='mm',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(exclude=["test*","build","dist"]),
     data_files = get_data_files("mm/bin"),
     install_requires=['Jinja2', 'suds==0.4', 'keyring', 'MarkupSafe==0.18', 'requests'],
